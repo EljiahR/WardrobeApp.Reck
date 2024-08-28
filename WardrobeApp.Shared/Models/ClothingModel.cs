90d3b5b679c3a909a71150268a839b1c4c4ac0bc
Elijah <kcerhajile@gmail.com>
2024-08-28 13:28:52 -0400

    Added Data Annotation Validation to EditForm and modified layout of displayed clothes

--- WardrobeApp.Shared/Models/ClothingModel.cs
+++ WardrobeApp.Shared/Models/ClothingModel.cs
@@ -11,6 +11,7 @@ public class ClothingModel
 {
     public int Id { get; set; }
     [Required]
+    [StringLength(50, MinimumLength = 3)]
     public string? Category { get; set; }
     public string? Description { get; set; }
     public byte[]? ImageData { get; set; }
